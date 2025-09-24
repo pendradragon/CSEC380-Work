ef6eee4ee44621a475f42dba6c609acd6ec68fe8
Megan <143452991+pendradragon@users.noreply.github.com>
2025-09-24 14:53:43 -0400

    DEBUG: Variable definitions and response parsing

--- Lab 03/Step01.py
+++ Lab 03/Step01.py
@@ -3,7 +3,8 @@ import socket
 
 #Using the provided information about the webhost
 target_port = 380
-target_host = "hw3.csec380.fun
+target_host = "hw3.csec380.fun"
+endpoint = "/hello"
 
 #the code provide in the beginning of the assignment
 CRFL = "\r\n"
@@ -11,6 +12,8 @@ CRFL = "\r\n"
 def calibrate(): 
   client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   client.connect((target_host, target_port))
+
+  
   #request generation
   firstline = "GET /hello HTTP/1.1" + CRFL
   request = CRFL + firstline
@@ -18,8 +21,7 @@ def calibrate():
   #response parsing
   httpresponse = client.recv(8192)
   response = httpresponse.decode()
-  httpresponse = client.recv(8192)
-  response = response + httpresponse
+
   client.close()
   print(response)
 
