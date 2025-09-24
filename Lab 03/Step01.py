2ee7d97b1f5f68aeeff0ca28165301df2bb9d453
Megan <143452991+pendradragon@users.noreply.github.com>
2025-09-24 14:19:23 -0400

    DEBUG: Added variable definitions
    target_port and target_host

--- Lab 03/Step01.py
+++ Lab 03/Step01.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 import socket
 
+#Using the provided information about the webhost
+target_port = 380
+target_host = "hw3.csec380.fun
+
 #the code provide in the beginning of the assignment
 CRFL = "\r\n"
 
 def calibrate(): 
   client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   client.connect((target_host, target_port))
+  #request generation
   firstline = "GET /hello HTTP/1.1" + CRFL
   request = CRFL + firstline
   client.send(request.encode())
+  #response parsing
   httpresponse = client.recv(8192)
   response = httpresponse.decode()
   httpresponse = client.recv(8192)
